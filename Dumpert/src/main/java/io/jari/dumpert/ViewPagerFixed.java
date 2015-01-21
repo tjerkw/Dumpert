f830a2c6780d4dd1b4535989d49f094e09a3bb35
JariZ <jarizw@gmail.com>
2015-01-21 14:43:31 +0100

    This should fix 'pointerIndex out of range', which is a internal android bug that can be ignored (bugfix)

---
+++ Dumpert/src/main/java/io/jari/dumpert/ViewPagerFixed.java
@@ -0,0 +1,42 @@
+/**
+ * ViewPager that ignores errors.
+ * https://github.com/chrisbanes/PhotoView/issues/31#issuecomment-19803926
+ * https://code.google.com/p/android/issues/detail?id=18990
+ */
+
+package io.jari.dumpert;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class ViewPagerFixed extends android.support.v4.view.ViewPager {
+
+    public ViewPagerFixed(Context context) {
+        super(context);
+    }
+
+    public ViewPagerFixed(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
